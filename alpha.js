b6fe89efa8245cd2ad88e4abeba286fead0d58d7
Russell Ries <riesru@gmail.com>
2016-04-19 14:39:49 -0500

    finish exercise

--- alpha.js
+++ alpha.js
@@ -9,3 +9,18 @@
 //Then add logic to stackLetters that places a space after every fifth letter,
 
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var currentStack = [];
+
+function stackLetters() {
+  for (var i = 0; i < alphabet.length; i++) {
+    if (i % 5 === 0) {
+      currentStack += " " + alphabet[i];
+      console.log(currentStack);
+    } else {
+      currentStack += alphabet[i];
+      console.log(currentStack);
+    }
+  }
+}
+
+stackLetters();
